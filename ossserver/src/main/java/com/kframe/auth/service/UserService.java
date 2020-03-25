c15725400a7f63392899423e1970ea9024754223
kunge2013 <543976974@qq.com>
2020-03-25 10:19:20 +0800

    12121

--- ossserver/src/main/java/com/kframe/auth/service/UserService.java
+++ ossserver/src/main/java/com/kframe/auth/service/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.kframe.bean.PageData;
